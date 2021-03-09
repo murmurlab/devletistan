d6e78f73dc1467c7794c43d735183d25cc664d21
murmurLAB <0aeonchannel0@gmail.com>
2021-03-10 00:53:09 +0300

    up

--- client.js
+++ client.js
@@ -2,3 +2,22 @@
 hesap açıldığında, hesabın onaylanması gerekir
 hesabın onaylanması için hesap 30lvl olması lazım 
 */
+function scrollToBottom (id) {
+    let div = document.getElementById(id);
+    div.scrollTop = div.scrollHeight/*- div.clientHeight;*/;
+ }
+scrollToBottom("chat")
+
+function send (msg){
+    document.getElementById("msg-box").value=""
+    z=document.createElement("dl")
+    x=document.createElement("dt")
+    b=document.createElement("br")
+    x.textContent="adsdsa"
+    z.appendChild(x)
+    document.getElementById("messages").appendChild(z)
+    document.getElementById("messages").appendChild(b)
+    scrollToBottom("chat")
+}
+
+document.getElementById("send").addEventListener("click",send)
