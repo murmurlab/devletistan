09315356b40ed676cc5ce6f72936304878622156
murmurLAB <0aeonchannel0@gmail.com>
2021-03-15 16:45:10 +0300

    update

---
+++ client/enter.js
@@ -0,0 +1,49 @@
+
+function onSignIn(googleUser) {
+
+    }   
+    
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    console.log('User signed out.');
+        document.cookie="usertoken="
+    });
+    }
+
+function onSuccess(googleUser) {
+    console.log("logining");
+    var id_token = googleUser.getAuthResponse().id_token;
+    var profile = googleUser.getBasicProfile();
+    console.log(profile);
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    let req1 = new XMLHttpRequest()
+    req1.open("POST", "http://4.tcp.ngrok.io:10186")
+    
+    req1.onload = function() {
+
+        //console.log('response: ' + req1.responseText);
+      }
+    
+    req1.send('idtoken=' + id_token);
+    console.log("token sended");
+    }
+function onFailure(error) {
+    console.log(error);
+    }
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+    }
+  
--- server.js
+++ server.js
@@ -1,32 +1,64 @@
 const http = require("http")
+const https = require("https")
 const fs =  require("fs")
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client("179093935575-hi380bs29g8ob1nbmhnv7vkl52nigmv8.apps.googleusercontent.com");
+const CLIENT_ID = "179093935575-hi380bs29g8ob1nbmhnv7vkl52nigmv8.apps.googleusercontent.com"
+const PORT = 80
+const SPORT = 443
+const LOCAL_HOST = "localhost"
+const WLAN_HOST = "192.168.1.101"
+
+
+
+const options = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem")
+}
 
 let dirs,
     file,
-    chars=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","q","w","x"]
-function filitre(id) {
+    token
+
+    function filitre(dt,type) {
+    charsid=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","q","w","x"]
+    charspass=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","q","w","x","!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", ":", "\"", ";", "'", "<", ">", "?", ",", ".", "/", "|", "\\"]
+    console.log("for pass a-z A-Z 0-9 !@#$%^&*()_+-={}[]:\";\'<>?,./|\\");
+    console.log("for id a-z A-Z 0-9");
+    console.log("filitre('mehmet&!#%','id')  or  filitre('ahmet%!&$','pass')");
     console.log("in filter");
-    if (!id=="") {
+    if (!dt=="") {
         console.log('not ""');
-        if (id.split("").every((e)=>{return chars.some(a=>{return e==a})})){
-            return true
+        if (type=="pass") {
+            if (dt.split("").every((e)=>{return charspass.some(a=>{return e==a})})){
+                return true
+            }else{
+                return false
+            }
+        }else if (type=="id") { 
+            if (dt.split("").every((e)=>{return charsid.some(a=>{return e==a})})){
+                return true
+            }else{
+                return false
+            }
         }
     }
 }
 
-let server = http.createServer((istek,yanıt)=>{
+reqHandler = (istek,yanıt)=>{
     
     let contentType = 'text/html'
     let ex ="html"
     dirs = fs.readdirSync("./client/")
 
     if (istek.method==="POST") {
-
+        let cookies = {},coki
+        
         let data = JSON.parse(fs.readFileSync("users.json").toString())
 
         console.log(istek.method,istek.url)
 
-        var body = "";
+        let body = "";
         istek.on("data", chunk=>{
             body += chunk;
         })
@@ -35,13 +67,109 @@ let server = http.createServer((istek,yanıt)=>{
         let user = {},to,method
          
         istek.on("end", ()=>{
-            
-            user[body.split("&")[1].split("=")[0]]=body.split("&")[1].split("=")[1]
-            user[body.split("&")[2].split("=")[0]]=body.split("&")[2].split("=")[1]
-            to=body.split("&")[3].split("=")[1]
-            method=body.split("&")[0].split("=")[1]
-            console.log(user,method,to)
-            end()
+            if (body.split("&").length==4) {
+                if (body.split("&")[0].split("=").length==2) {
+                    if (body.split("&")[0].split("=")[0]=="method") {
+                        if (body.split("&")[1].split("=").length==2) {
+                            if (body.split("&")[1].split("=")[0]=="id") {
+                                if (body.split("&")[2].split("=").length==2) {
+                                    if (body.split("&")[2].split("=")[0]=="pass") {
+                                        if (body.split("&")[3].split("=").length==2) {
+                                            if (body.split("&")[3].split("=")[0]=="where") {
+                                                console.log(body)
+                                                console.log("decoded   ",decodeURIComponent(body))
+                                                user[body.split("&")[1].split("=")[0]]=body.split("&")[1].split("=")[1]
+                                                user[body.split("&")[2].split("=")[0]]=body.split("&")[2].split("=")[1]
+                                                to=body.split("&")[3].split("=")[1]
+                                                method=body.split("&")[0].split("=")[1]
+                                                console.log(user,method,to)
+                                                end()
+                                            }else{
+                                                console.log("en az => method=&id=&pass=&where=");
+                                            }
+                                        }else{
+                                            console.log("en az => method=&id=&pass=&x=");
+                                        }
+                                    }else{
+                                        console.log("en az => method=&id=&pass=&");
+                                    }
+                                }else{
+                                    console.log("en az => method=&id=&x=&");
+                                }
+                            }else{
+                                console.log("en az => method=&id=&&");
+                            }
+                        }else{
+                            console.log("en az => method=&x=&&");
+                        }
+                    }else{
+                        console.log("en az => method=&&&");
+                    }
+                }else{
+                    console.log("en az => x=&&&");
+                }
+            }else{
+                    istek.headers.cookie.split(";").forEach(element => {
+                    cookies[element.split("=")[0]]=element.split("=")[1]
+                });
+                //console.log(cookies);
+
+                async function verify() {
+
+                    //console.log(body)
+                    token=body.split("=")[1]
+                    //console.log(token)
+
+                    const ticket = await client.verifyIdToken({
+                        idToken: token,
+                        audience: CLIENT_ID,  
+                        // Specify the CLIENT_ID of the app that accesses the backend
+                        // Or, if multiple clients access the backend:
+                        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+                    })
+
+                    const payload = ticket.getPayload()
+                    const userid = payload['sub']
+                    // If request specified a G Suite domain:
+                    // const domain = payload['hd'];
+                    console.log(payload,userid);
+
+                    yanıt.writeHead(200, {
+                        'Set-Cookie': "usertoken="+token,
+                        'Content-Type': 'text/plain'
+                        })
+                    //WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWw
+                    if(!data.users[token]){
+
+                    data.users[token]={
+                        "name": payload["name"]
+                    }
+                    fs.writeFileSync("users.json",JSON.stringify(data,null,2))
+                    console.log("kayıt edildi")
+                    
+                    if(dirs.find(a=>a=="game.html")){
+                        return yanıt.end(fs.readFileSync("client/game.html"))
+                        }else{
+
+                        }
+
+                        }else{
+                            if(dirs.find(a=>a=="game.html")){
+                                //get isteğind ehtml response edemiyon
+                                yanıt.end(fs.readFileSync("client/game.html"))
+                                }else{
+    
+                            }
+                        }
+                    }
+                    verify().catch((e)=>{
+                        console.error(e)
+                        yanıt.end("cant verified, wrong cookie!!!!!!!!!!!!")
+                    })
+                    
+                  
+                
+            }
         })
         function end() {
             if (method=="login") {
@@ -77,7 +205,7 @@ let server = http.createServer((istek,yanıt)=>{
                     } */
                 }
             }else{
-                if (filitre(user.id)){
+                if (filitre(user.id,"id")&&filitre(user.pass,"pass")){
                     if (data.users[user.id]) {
                         console.log("id zaten kayıtlı")
                         yanıt.end("id zaten kayıtlı")
@@ -85,15 +213,15 @@ let server = http.createServer((istek,yanıt)=>{
                         data.users[user.id]={
                             "pass":""
                         }
-                        data.users[user.id]["pass"]=user.pass
+                        data.users[user.id]["pass"]=decodeURIComponent(user.pass)
                         fs.writeFileSync("users.json",JSON.stringify(data,null,2))
                         
                         console.log("kayıt edildi")
                         yanıt.end("kayıt edildi")
                     }
                 }else{
-                    console.log("geçersiz id")
-                    yanıt.end("geçersiz id")
+                    console.log("geçersiz id veya parola")
+                    yanıt.end("geçersiz id veya parola")
                     
                 }
             }
@@ -103,6 +231,7 @@ let server = http.createServer((istek,yanıt)=>{
         console.log(istek.method,istek.url)
     }
     if (istek.method==="GET") {
+        
         if (istek.url=="/") {
             if(dirs.find(a=>a=="enter.html")){
                 return yanıt.end(fs.readFileSync("client/enter.html"))
@@ -145,8 +274,10 @@ let server = http.createServer((istek,yanıt)=>{
 
  
 
-})
-const socket = require("socket.io")(server)
+}
+const httpsServer = https.createServer(options, reqHandler)
+const httpServer = http.createServer(reqHandler)
+const socket = require("socket.io")(httpsServer)
 
 socket.on("connection",client=>{
     client.on("msg",msg=>{
@@ -154,4 +285,5 @@ socket.on("connection",client=>{
     })
 })
 
-server.listen(80,"192.168.1.101",()=>console.log("ok"))
+httpServer.listen(PORT,WLAN_HOST,()=>console.log("ok http"))
+httpsServer.listen(SPORT,WLAN_HOST,()=>console.log("ok https"))
